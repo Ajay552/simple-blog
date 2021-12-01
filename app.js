ab9d1c8ea4a5dffe52c246873523cd518dddf3f1
ajay_S <sajayyy01234@gmail.com>
2021-12-01 21:41:56 +0530

    added views with ejs

--- app.js
+++ app.js
@@ -15,22 +15,35 @@ app.listen(3000);
 app.get("/", (req,res) => {
     //automatically sets the content type and also sets the sataus code
     // res.send("<p> Home page</p>");
-    res.sendFile("./views/index.html", {root: __dirname});
+
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+
+    res.render("index", { title: "Home", blogs: blogs});
 });
 
 app.get("/about", (req,res) => {
     //automatically sets the content type and also sets the sataus code
     // res.send("<p> Home page</p>");
-    res.sendFile("./views/about.html", {root: __dirname});
+    // res.sendFile("./views/about.html", {root: __dirname});
+    res.render("about", { title: "about"});
 });
 
 // redirect page
-app.get("/about-us", (req,res) => {
-    res.redirect("/about");
+// app.get("/about-us", (req,res) => {
+//     res.redirect("/about");
+// });
+
+app.get("/blogs/create", (req,res) => {
+    res.render("create", { title: "create"});
 });
 
 // 404 page
 // and this code must be at the bottom
 app.use((req,res) => {
-    res.status(400).sendFile("./views/404.html", { root: __dirname});
+    // res.status(400).sendFile("./views/404.html", { root: __dirname});
+    res.render("404", { title: "404"});
 });
